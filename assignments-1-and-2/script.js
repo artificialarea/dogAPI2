6289278b1312ac7242fc368bfecdaac391389259
Sacha Sedriks <sachased@gmail.com>
2020-04-11 13:35:42 -0700

    created two directories to seperate assignments. Root index.html is now a table of contents that links to each of the assignments.

---
+++ assignments-1-and-2/script.js
@@ -0,0 +1,111 @@
+'use strict';
+/* eslint-disable indent */
+
+// API doc: https://dog.ceo/dog-api/documentation/
+
+function init() {
+  handleSubmission();
+
+}
+
+//////////////////////////////////////////////////////////////
+// SEPERATION OF CONCERNS: TYPES OF FUNCTIONS
+// (Miscellaneous): Fetch Data
+// Template Generators
+// Rendering Functions
+// Event Handlers
+//////////////////////////////////////////////////////////////
+
+
+// MISCELLANEOUS /////////////////////////////////////////////
+
+function fetchDogImages(num) {
+  // fetch then render each dog/img *individually* to the DOM
+  // thus limiting the fetch promise to one image at a time
+  for (let i = 1; i <= num; i++) {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(data => {
+        // Assignment 1: print results of image output.
+        console.log(data.message); 
+        // Assignment 2: render those images onto the DOM.
+        renderDogImage(data.message);
+      })
+      .catch(err => console.log('error'));
+  }
+}
+
+
+
+// TEMPLATE GENERATORS ///////////////////////////////////////
+
+function generateDogImage(data) {
+  // generate a (single) dog image
+  return `
+    <img src="${data}" class="">
+  `;
+}
+
+function generateErrorMsg(data) {
+  if (data <= 0) {
+    // Assignment 1: print error msg. 
+    console.log("ERROR: Don't you want to see any dogs? Please submit another number...");
+    // Assignment 2: render this error msg onto the DOM.
+    return `
+      <p class="error">Don't you want to see any dogs? Please submit another number...</p>
+    `;
+
+  } else {
+    // Assignment 1: print error msg.
+    console.log("ERROR: We can only display up to 50 dog images. No more than that, I'm afraid. Please submit another number...");
+    // Assignment 2: render this error msg onto the DOM.
+    return `
+      <p class="error">We can only display up to 50 dog images. No more than that, I'm afraid. Please submit another number...</p>
+    `;
+  }
+}
+
+
+
+// RENDERING FUNCTIONS ///////////////////////////////////////
+
+function renderDogImage(data) {
+  // generate HTML to render
+  const dogHTML = generateDogImage(data);
+
+  // render HTML into DOM
+  $('.js-image-gallery').append(dogHTML);
+  $('.js-image-gallery').removeClass('hidden');
+}
+
+function renderErrorMsg(data) {
+  const errMsg = generateErrorMsg(data);
+  $('.js-image-gallery').html(errMsg);
+  $('.js-image-gallery').removeClass('hidden');
+}
+
+
+
+// EVENT HANDLERS ////////////////////////////////////////////
+
+function handleSubmission() {
+  // get number submitted via form input
+  $('#dog-form').submit( event => {
+    event.preventDefault();
+    const numDogs = $('.js-dog-generator-entry').val();
+    
+    // reset DOM, if a prior submission already occurred
+    $('.js-image-gallery').html('');
+    
+    if (numDogs <= 0 || numDogs > 50) { // limit to 50 images
+      renderErrorMsg(numDogs);
+    } else {
+      fetchDogImages(numDogs);
+    }
+  });
+}
+
+
+
+// on page load
+$(init);
