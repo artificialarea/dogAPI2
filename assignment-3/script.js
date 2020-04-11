06be058b1cc5d64ef55c3b0c5ef7d0981557d125
Sacha Sedriks <sachased@gmail.com>
2020-04-11 15:22:55 -0700

    Assignment 3 DONE.

--- assignment-3/script.js
+++ assignment-3/script.js
@@ -4,8 +4,8 @@
 // API doc: https://dog.ceo/dog-api/documentation/
 
 function init() {
+  fetchAllBreeds();               // to populate dropdown menu
   handleSubmission();
-
 }
 
 //////////////////////////////////////////////////////////////
@@ -19,68 +19,73 @@ function init() {
 
 // MISCELLANEOUS /////////////////////////////////////////////
 
-function fetchDogImages(num) {
-  // fetch then render each dog/img *individually* to the DOM
-  // thus limiting the fetch promise to one image at a time
-  for (let i = 1; i <= num; i++) {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(data => {
-        // Assignment 1: print results of image output.
-        console.log(data.message); 
-        // Assignment 2: render those images onto the DOM.
-        renderDogImage(data.message);
-      })
-      .catch(err => console.log('error'));
-  }
+function fetchAllBreeds() {
+  fetch('https://dog.ceo/api/breeds/list/all')
+    .then(response => response.json())
+    .then(data => {
+      // console.log(data.message); 
+      renderDropdown(data.message);
+    })
+    .catch(err => console.log('error fetchAllBreeds'));
+}
+
+function fetchDogImage(breed) {
+  fetch(`https://dog.ceo/api/breed/${breed}/images/random`)
+    .then(response => {
+      // NOTE: The Promise returned from fetch() won’t reject on HTTP error status even if the response is an HTTP 404 or 500. Instead, it will resolve normally (with ok status set to false), and it will only reject on network failure or if anything prevented the request from completing.
+      // The good is news is fetch provides a simple ok flag that indicates whether an HTTP response’s status code is in the successful range or not. 
+      // SOLUTION thanks to: https://www.tjvantoll.com/2015/09/13/fetch-and-errors/
+      if (!response.ok) {
+        throw Error(response.statusText);
+      }
+      return response.json();
+    })
+    .then(data => {
+      console.log(data.message); 
+      renderDogImage(data.message);
+    })
+    .catch(err => console.log('ERROR: that breed doesn\'t exist'));
 }
 
 
 
 // TEMPLATE GENERATORS ///////////////////////////////////////
 
-function generateDogImage(data) {
-  // generate a (single) dog image
+function generateBreedOptions(breeds) {
+  // create a new array extracting all the keys 
+  // within the data.message object
+  const options = Object.keys(breeds).map(breed => {
+    return `<option value="${breed}">${breed}</option>`;
+  });
+
   return `
-    <img src="${data}" class="">
+    <option value="" disabled selected>select dog breed</option>
+    <option value-"breed that doesn't exist">breed that doesn't exist (throw error)</option>
+    ${options}
   `;
 }
 
-function generateErrorMsg(data) {
-  if (data <= 0) {
-    // Assignment 1: print error msg. 
-    console.log("ERROR: Don't you want to see any dogs? Please submit another number...");
-    // Assignment 2: render this error msg onto the DOM.
-    return `
-      <p class="error">Don't you want to see any dogs? Please submit another number...</p>
-    `;
-
-  } else {
-    // Assignment 1: print error msg.
-    console.log("ERROR: We can only display up to 50 dog images. No more than that, I'm afraid. Please submit another number...");
-    // Assignment 2: render this error msg onto the DOM.
-    return `
-      <p class="error">We can only display up to 50 dog images. No more than that, I'm afraid. Please submit another number...</p>
-    `;
-  }
+function generateDogImage(data) {
+  return `
+    <img src="${data}" class="">
+  `;
 }
 
 
 
 // RENDERING FUNCTIONS ///////////////////////////////////////
 
+function renderDropdown(breeds) {
+  const dropdownHTML = generateBreedOptions(breeds);
+  $('#js-breed').html(dropdownHTML);
+}
+
 function renderDogImage(data) {
   // generate HTML to render
   const dogHTML = generateDogImage(data);
 
   // render HTML into DOM
-  $('.js-image-gallery').append(dogHTML);
-  $('.js-image-gallery').removeClass('hidden');
-}
-
-function renderErrorMsg(data) {
-  const errMsg = generateErrorMsg(data);
-  $('.js-image-gallery').html(errMsg);
+  $('.js-image-gallery').html(dogHTML);
   $('.js-image-gallery').removeClass('hidden');
 }
 
@@ -89,19 +94,9 @@ function renderErrorMsg(data) {
 // EVENT HANDLERS ////////////////////////////////////////////
 
 function handleSubmission() {
-  // get number submitted via form input
-  $('#dog-form').submit( event => {
-    event.preventDefault();
-    const numDogs = $('.js-dog-generator-entry').val();
-    
-    // reset DOM, if a prior submission already occurred
-    $('.js-image-gallery').html('');
-    
-    if (numDogs <= 0 || numDogs > 50) { // limit to 50 images
-      renderErrorMsg(numDogs);
-    } else {
-      fetchDogImages(numDogs);
-    }
+  $('#dog-form').on('change', event => {
+    const selectedBreed = $('#js-breed').val();
+    fetchDogImage(selectedBreed);
   });
 }
 
